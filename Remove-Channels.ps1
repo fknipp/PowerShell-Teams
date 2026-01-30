cc02f1c4dea428cae27682f765b86be461ec3014
Franz Knipp <franz@qnipp.com>
2026-01-30 21:07:14 +0100

    Increase verbosity

--- Remove-Channels.ps1
+++ Remove-Channels.ps1
@@ -4,7 +4,9 @@ param(
 )
 
 Get-TeamAllChannel -GroupId $GroupId | Where-Object { $_.DisplayName -ne "General" } | ForEach-Object {
-  $NewName = "{0:X8}.{1,-35}" -f (Get-Random), $_.DisplayName 
+  $NewName = ("{0:X8}.{1,-35}" -f (Get-Random), $_.DisplayName).Trim()
+  Write-Host ("Renaming channel {0} to {1}." -f $_.DisplayName, $NewName)
   Set-TeamChannel -GroupId $GroupId -CurrentDisplayName $_.DisplayName -NewDisplayName $NewName
+  Write-Host ("Removing channel {0}." -f $NewName)
   Remove-TeamChannel -GroupId $GroupId -DisplayName $NewName
 }
